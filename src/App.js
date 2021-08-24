2358667988d5e86e86fffe67d9a0c8760e9cf157
rahul rajput <rahulrajput7358@gmail.com>
2021-08-25 01:04:39 +0530

    working on carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App default css/App.css";
 import { Routes } from "./components/Route component/Routes";
+import MuiCarousel from "./components/material-ui-components/MuiCarousel";
 function App() {
   return (
     <div>
       <Routes />
+      <MuiCarousel />
     </div>
   );
 }
