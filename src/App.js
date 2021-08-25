23435f01b57eb4b87ae48a55c87f0d3dc2f5a1ca
Akshaykumar <akshaykumarpurad99@gmail.com>
2021-08-25 10:48:29 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App default css/App.css";
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/navbar/NavBar";
 import { Routes } from "./components/Route component/Routes";
 function App() {
   return (
     <div>
       <Routes />
+      <NavBar />
+      <Footer />
     </div>
   );
 }
