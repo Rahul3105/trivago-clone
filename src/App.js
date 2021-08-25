e02dacebf18fb8d448ffb4a70c70d23869dc31c3
Akshaykumar <akshaykumarpurad99@gmail.com>
2021-08-26 02:30:27 +0530

    Hotel Card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App default css/App.css";
-import Footer from "./components/footer/Footer";
-import NavBar from "./components/navbar/NavBar";
+// import Footer from "./components/footer/Footer";
+// import NavBar from "./components/navbar/NavBar";
 import { Routes } from "./components/Route component/Routes";
-import MuiCarousel from "./components/material-ui-components/MuiCarousel";
+// import MuiCarousel from "./components/material-ui-components/MuiCarousel";
+import HotCard from "./components/HotDet/HotCard";
 function App() {
   return (
     <div>
       <Routes />
-      <NavBar />
-      <Footer />
-
-      <MuiCarousel />
+      {/* <NavBar /> */}
+      <HotCard />
+      {/* <Footer /> */}
     </div>
   );
 }
