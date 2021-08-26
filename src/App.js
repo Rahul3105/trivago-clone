61e575c429f3b03286771f5c72a2e4065a9b3e2f
rahul rajput <rahulrajput7358@gmail.com>
2021-08-27 00:36:13 +0530

    checkout-page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App default css/App.css";
 import { useSelector } from "react-redux";
 import { Routes } from "./components/Route component/Routes";
 import Loading from "./components/material-ui-components/LoadingAnimation";
+import { CheckoutPage } from "./components/Checkout-Page/CheckoutPage";
 function App() {
   const login = useSelector((state) => state.login);
   return (
     <div>
-      {login.isLoading && <Loading />}
-      <Routes />
+      {/* {login.isLoading && <Loading />} */}
+      {/* <Routes /> */}
+      <CheckoutPage />
     </div>
   );
 }
