433d45a4fe3684d7c2bfd59bdffcc52767bf6696
rahul rajput <rahulrajput7358@gmail.com>
2021-08-28 00:31:35 +0530

    home-carousel-fixed

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import NavBar from "./components/navbar/NavBar";
 import "./App default css/App.css";
 import { useSelector } from "react-redux";
 import { Routes } from "./components/Route component/Routes";
-// import MuiCarousel from "./components/material-ui-components/MuiCarousel";
+
 import HotCard from "./components/HotDet/HotCard";
 
 import Loading from "./components/material-ui-components/LoadingAnimation";
-import { CheckoutPage } from "./components/Checkout-Page/CheckoutPage";
+// import { CheckoutPage } from "./components/Checkout-Page/CheckoutPage";
 
-import { MapComponent } from "./components/Map/MapComponent";
+// import { MapComponent } from "./components/Map/MapComponent";
 
-import { ImageSlideShow } from "./components/Image SlideShow/ImageSlideShow";
-import { ImageSliderData } from "./components/Image SlideShow/ImageSliderData";
+// import { ImageSlideShow } from "./components/Image SlideShow/ImageSlideShow";
+// import { ImageSliderData } from "./components/Image SlideShow/ImageSliderData";
 function App() {
   const login = useSelector((state) => state.login);
   return (
     <div>
       {login.isLoading && <Loading />}
-      {/* <Routes /> */}
-      <ImageSlideShow slides={ImageSliderData} />
+      <Routes />
+      {/* <ImageSlideShow slides={ImageSliderData} /> */}
       {/* <MapComponent /> */}
       {/* <CheckoutPage /> */}
     </div>
