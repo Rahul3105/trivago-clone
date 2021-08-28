a5a52fe8038b92a3cdae4642931d745234bf2d66
Akshaykumar <akshaykumarpurad99@gmail.com>
2021-08-28 11:00:10 +0530

    1

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -1,9 +1,11 @@
-// import { GoogleMap, withScriptjs,withGoogleMap } from 'react-google-maps';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-// const Map = () => {
-//     return <GoogleMap
-//         defaultZoom={18}
-//         defaultCenter={{ lat: 28.644800, lng: 77.216721 }}
-//     />
-// }
-// export const WrapperMap = withScriptjs(withGoogleMap(Map));
+const Map = () => {
+  return (
+    <GoogleMap
+      defaultZoom={18}
+      defaultCenter={{ lat: 28.6448, lng: 77.216721 }}
+    />
+  );
+};
+export const WrapperMap = withScriptjs(withGoogleMap(Map));
