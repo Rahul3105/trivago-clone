81c5f3c4efd256c8ec4ef6fa693483fe6b16ba41
rahul rajput <rahulrajput7358@gmail.com>
2021-08-29 17:46:10 +0530

    pulling

--- src/App.js
+++ src/App.js
@@ -10,32 +10,14 @@ import { MoreFilterCard } from "./components/material-ui-components/MoreFilterCa
 import { SearchBar } from "./components/Search-Bar/SearchBar";
 import { FilterSearchBar } from "./components/FilterSearchBar/FilterSearchBar";
 import BigHot from "./components/HotDet/BigHot";
-// import { CheckoutPage } from "./components/Checkout-Page/CheckoutPage";
-
-// import { MapComponent } from "./components/Map/MapComponent";
-
-// import { ImageSlideShow } from "./components/Image SlideShow/ImageSlideShow";
-// import { ImageSliderData } from "./components/Image SlideShow/ImageSliderData";
+import Loading from "./components/material-ui-components/LoadingAnimation";
 
 function App() {
   const login = useSelector((state) => state.login);
   return (
     <div>
-      {/* {login.isLoading && <Loading />} */}
-      {/* <ImageSlideShow slides={ImageSliderData} /> */}
-      {/* <MapComponent /> */}
-
-      {/* <CheckoutPage /> */}
-      {/* <LocationCard />
-      <MoreFilterCard />
-      <RatingCard /> */}
-
-      {/* <FilterSearchBar /> */}
-
-      {/* <FilterSearchBar />  */}
+      {login.isLoading && <Loading />}
       <Routes />
-
-      {/* <FilterSearchBar /> */}
     </div>
   );
 }
